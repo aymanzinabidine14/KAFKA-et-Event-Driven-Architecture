80ee85606879be472de4d7c6b5d1ffbc481dc0b9
aymanzinabidine14 <Ayman.zinabidine@emsi-edu.ma>
2023-12-31 23:30:55 +0100

    v1

---
+++ src/main/java/org/sid/demospringcloudstreamskafka/entities/PageEvent.java
@@ -0,0 +1,16 @@
+package org.sid.demospringcloudstreamskafka.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class PageEvent {
+    private String name;
+    private String user;
+    private Date date;
+    private long duration;
+}
